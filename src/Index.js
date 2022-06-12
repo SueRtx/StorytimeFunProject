6855b56acd1c3cb682c0b96ced83f7533475d8f1
Adrienne <100542795+ampando@users.noreply.github.com>
2022-06-12 12:40:48 -0700

    WIP index.js co-authored-by:Sun Roberts <suerobertstx@yahoo.com> co-authored-by:Sean Julius <crimsonsand@live.com> co-authored-by:Lilia Krivelyova <lilia.krivelyova@gmail.com>

--- src/Index.js
+++ src/Index.js
@@ -5,31 +5,14 @@ import './css/styles.css';
 //import WeatherService from './weather-service.js';
 
 $(document).ready(function() {
-  $('#pP1').click(function() {
-    $("#page1").hide();
-    $("#poxyPath1").show();
-  });
+
   $('#tP1').click(function(){
     $('#page1').hide();
-    $('#tinyPath1').show();
-  });
-  $('#tinyPicnic2').click(function(){
-    $('#tinyPath1').hide();
-    $('#tinyPicnic').show();
+    $('#tinyPath').show();
   });
 
-  $('#tinyDragonPic').click(function(){
-   // $('#tinyPicnic').hide();
-    $('#tinyDragonPicnic').show();
+  $('#pP1').click(function(){
+    $('#page1').hide();
+    $('#poxyPath').show();
   });
-
-
-  $("#tinyBeach2").click(function(){
-    $("#tinyPicnic").hide();
-    $('#tinyBeach2').show();
-  });
-  /*$("#tinyBeach").click(function(){
-  $('#tinyBeach2').hide();
-    $('#')
-  }*/
 });
