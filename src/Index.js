69524f19a7a8b5f903b2db6b14da72941386c354
Adrienne <100542795+ampando@users.noreply.github.com>
2022-06-12 10:31:42 -0700

    fix indentation

--- src/Index.js
+++ src/Index.js
@@ -4,9 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 //import WeatherService from './weather-service.js';
 
-
-
-
 $(document).ready(function() {
   $('#pP1').click(function() {
     $("#page1").hide();
