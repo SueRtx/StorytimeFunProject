4c44ecac4f15ccc7803888bb39afaa01c5d979f4
Adrienne <100542795+ampando@users.noreply.github.com>
2022-06-12 17:23:26 -0700

    WIP change index.
    co-authored-by:Sun Roberts <suerobertstx@yahoo.com>
    
    co-authored-by:Sean Julius <crimsonsand@live.com>co-authored-by:Lilia Krivelyova <lilia.krivelyova@gmail.com>

--- src/Index.js
+++ src/Index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
+/*tiny picnic/dragon path */
 
 $(document).ready(function() {
   $('#poxyPicnic').hide();
@@ -29,6 +30,28 @@ $(document).ready(function() {
     $('#page1').show();
   });
   
+  /*tiny picnic/unicorn path */
+
+  $('#tiny').click(function(){
+    $('#page1').hide();
+    $('#tinyPath').show();
+  });
+
+  $('#tinyPicnic1').click(function(){
+    $('#tinyPath').hide();
+    $('#tinyPicnic').show();
+  });
+
+  $('#tinyUnicorn1').click(function(){
+    $('#tinyPicnic').hide();
+    $('#tinyUnicornPic').show();
+  });
+
+  $('#end').click(function(){
+    $('#tinyUnicornPic').hide();
+    $('#page1').show();
+  });
+
   /* Poxy Picnic/Dragon Path Starts*/
 
   $('#poxy').click(function(){
@@ -50,6 +73,73 @@ $(document).ready(function() {
     $('#poxyDragonPic').hide();
     $('#page1').show();
   });
+  
+  /* Poxy Picnic/Unicorn Path Starts*/
+  
+  $('#poxy').click(function(){
+    $('#page1').hide();
+    $('#poxyPath').show();
+  });
+
+  $('#poxyPicnic1').click(function(){
+    $('#poxyPath').hide();
+    $('#poxyPicnic').show();
+  });
+
+  $('#poxyUnicorn1').click(function(){
+    $('#poxyPicnic').hide();
+    $('#poxyUnicornPic').show();
+  });
+
+  $('#end1').click(function(){
+    $('#poxyUnicornPic').hide();
+    $('#page1').show();
+  
+  });
+  
+  /* Tiny Beach/Dragon Path Starts*/
+  
+  $('#tiny').click(function(){
+    $('#page1').hide();
+    $('#tinyPath').show();
+  });
+
+  $('#tinySwim1').click(function(){
+    $('#tinyPath').hide();
+    $('#tinySwim').show();
+  });
+
+  $('#dragon1').click(function(){
+    $('#tinySwim').hide();
+    $('#tinyDragonSwim').show();
+  });
+
+  $('#end5').click(function(){
+    $('#tinyDragonSwim').hide();
+    $('#page1').show();
+  });
+
+  /* Tiny Beach/Unicorn Path Starts*/
+
+  $('#tiny').click(function(){
+    $('#page1').hide();
+    $('#tinyPath').show();
+});
+
+  $('#tinySwim1').click(function(){
+    $('#tinyPath').hide();
+    $('#tinySwim').show();
+});
+
+  $('#unicorn').click(function(){
+    $('#tinySwim').hide();
+    $('#tinyUnicornSwim').show();
+});
+
+  $('#end2').click(function(){
+    $('#tinyUnicornSwim').hide();
+    $('#page1').show();
+});
   
   /* Poxy Beach/Dragon Path Starts*/
   
@@ -92,28 +182,6 @@ $(document).ready(function() {
 
   $('#end4').click(function(){
     $('#poxyUnicornSwim').hide();
-    $('#page1').show();
-  });
-
-  /* Tiny Beach/Dragon Path Starts*/
-  
-  $('#tiny').click(function(){
-    $('#page1').hide();
-    $('#tinyPath').show();
-  });
-
-  $('#tinySwim1').click(function(){
-    $('#tinyPath').hide();
-    $('#tinySwim').show();
-  });
-
-  $('#dragon1').click(function(){
-    $('#tinySwim').hide();
-    $('#tinyDragonSwim').show();
-  });
-
-  $('#end5').click(function(){
-    $('#tinyDragonSwim').hide();
     $('#page1').show();
   });
 });
